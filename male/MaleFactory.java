773deeb3cbea3ef0c753b89674ab687f4df961de
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-07-12 12:38:37 +0300

    2.

--- male/MaleFactory.java
+++ male/MaleFactory.java
@@ -4,12 +4,12 @@ import com.javarush.task.task37.task3702.Human;
 
 public class MaleFactory {
 
-    public static Human getPerson(int age){
+    public Human getPerson(int age){
+        if (age > KidBoy.MAX_AGE && age <= TeenBoy.MAX_AGE){
+            return new TeenBoy();
+        }
         if (age <= KidBoy.MAX_AGE){
             return new KidBoy();
-        }
-        if (age <= TeenBoy.MAX_AGE){
-            return new TeenBoy();
         }
         return new Man();
     }
